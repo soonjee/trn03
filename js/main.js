425ad58bca559934c0696da70bc90c8828c2ab88
soonji <youinxxs@naver.com>
2022-07-27 15:42:13 +0900

    20220727

---
+++ js/main.js
@@ -0,0 +1,15 @@
+$(function () {
+    $('.TopBanner i').on('click', function () {
+        $('.TopBanner').slideUp();
+    })
+
+    $('.pop button').on('click', function () {
+        // $(this).parent().hide(); 하나씩 눌러 지우려면
+        $(this).parent().hide();
+    })
+
+
+
+
+
+});
